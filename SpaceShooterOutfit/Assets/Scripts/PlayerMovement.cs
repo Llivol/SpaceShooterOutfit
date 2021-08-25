ebc8f4c6f1a7a939e996e845361973500db047b1
llivol <adriapuigdellivol@gmail.com>
2021-08-25 17:22:56 +0200

    player movement

--- SpaceShooterOutfit/Assets/Scripts/PlayerMovement.cs
+++ SpaceShooterOutfit/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float rotSpeed;
+    [SerializeField] private float moveSpeed;
     private Rigidbody rb;
 
     private void Awake()
@@ -14,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        move();
         rotate();
     }
 
@@ -22,5 +24,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 rotation = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         Vector3 newForward = Vector3.RotateTowards(transform.forward, rotation, rotSpeed * Time.deltaTime, 0);
         rb.MoveRotation(Quaternion.LookRotation(newForward));
+    }
+
+    private void move()
+    {
+        Vector3 movement = transform.forward * Input.GetAxis("Thrust");
+        rb.MovePosition(transform.position + (movement * moveSpeed * Time.deltaTime));
     }
 }
