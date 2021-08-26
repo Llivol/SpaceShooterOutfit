d6697f0e028d588d9f49768846c98ced200688d7
llivol <adriapuigdellivol@gmail.com>
2021-08-26 12:56:10 +0200

    add player shield

--- SpaceShooterOutfit/Assets/Scripts/PlayerStats.cs
+++ SpaceShooterOutfit/Assets/Scripts/PlayerStats.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStats : Stats
-{   
+{
     public int MaxAmmo;
+    private bool hasShield;
+    [SerializeField] private GameObject shield;
     [SerializeField] private Slider healthBar;
-    
 
     protected override void Awake()
     {
         base.Awake();
         healthBar.maxValue = maxHitPoints;
         healthBar.value = maxHitPoints;
+        hasShield = shield.activeSelf;
     }
 
     public override void TakeDamage(int damage)
     {
-        base.TakeDamage(damage);
+        if (hasShield) SetShield(false);
+        else
+        {
+            base.TakeDamage(damage);
+            healthBar.value = hitPoints;
+        }
 
-        healthBar.value = hitPoints;
+    }
+
+    public void SetShield(bool value)
+    {
+        hasShield = value;
+        shield.SetActive(value);
     }
 }
