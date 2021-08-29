799e1700f9afeeda6a8871f7b55b09df3c0998d6
llivol <adriapuigdellivol@gmail.com>
2021-08-29 10:01:58 +0200

    asteroid spawner

---
+++ SpaceShooterOutfit/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] asteroidPrefabs;
+    [SerializeField] private int amount;
+    [SerializeField] private float maxDistance;
+
+    void Start()
+    {
+        int asteroidsCount = asteroidPrefabs.Length;
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject asteroidPrefab = asteroidPrefabs[Random.Range(0, asteroidsCount - 1)];
+            var newAsteroid = Instantiate(asteroidPrefab, Random.insideUnitSphere * maxDistance, Random.rotation);
+            newAsteroid.transform.SetParent(transform);
+        }
+    }
+}
